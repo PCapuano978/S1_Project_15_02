4bebf106544d1f99e3dd084b4d703efabfcdec67
Unknown <pcapuano978@west-mec.org>
2019-04-19 15:25:06 -0700

    Project 2 Started
    Most of today (after the MTA practice, that is) was spent as a learning experience trying figure out how to work regular expressions into form patterns. Coming from all that time spent, I can luckily say that I have found out a great deal of understanding (in resources as well) how I can properly input these. So, while I might not have gotten that much progress in terms of code, I can still safety walk away from today in saying that there was a great deal of progress mentally. 
    
    I may continue to work on this more over the weekend and I could even finish it to get on track, but considering how Sunday would be my only day to do so and I have other things planned still, I'm probably going to push this one off a bit later than that.

--- dl_expense.js
+++ dl_expense.js
@@ -5,8 +5,8 @@
    Tutorial 13
    Case Problem 2
 
-   Author: 
-   Date:   
+   Author: Patrick M. Capuano
+   Date:   4.19.19
    
    Filename: dl_expenses.js
    
@@ -39,10 +39,15 @@
 
 
 function formatNumber(val, decimals) {
-   return val.toLocaleString(undefined, {minimumFractionDigits: decimals, 
-                                         maximumFractionDigits: decimals});
+      return val.toLocaleString(undefined, {
+            minimumFractionDigits: decimals,
+            maximumFractionDigits: decimals
+      });
 }
 
 function formatUSCurrency(val) {
-   return val.toLocaleString('en-US', {style: "currency", currency: "USD"} );
+      return val.toLocaleString('en-US', {
+            style: "currency",
+            currency: "USD"
+      });
 }
