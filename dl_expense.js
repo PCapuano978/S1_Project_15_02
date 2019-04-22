7f311e33b077c366d432b194dde8b328bee2b306
Unknown <pcapuano978@west-mec.org>
2019-04-22 15:23:00 -0700

    Project Finished; Ready for Review
    Although no work was done over the weekend like I initially was skeptical of, I was able to finish the rest of it in today's session, including all necessary documentation (which I should have been perfectly capable of doing). The website itself even seems to work perfectly good with all the regular expressions placed in on Friday, so with that said, I believe it's safe to say this project as a whole is 100% complete (and ready for review).

--- dl_expense.js
+++ dl_expense.js
@@ -31,7 +31,94 @@
       
 */
 
+// An event listener was added that triggers on load in with an anynomous function that...
 
+window.addEventListener("load", function {
+
+
+      // ...creates a variable (changingCells) set to the input tags within the travelExp elements with the sum class. 
+      var changingCells = document.travelExp.sum.getElementsByTagName("input");
+
+      // And a for loop is created that goes through the changingCells collection and for every item...
+      for (var i = 0; i < changingCells.length; i++) {
+
+            // Runs the changingCells variable on change with the calcExp function.
+            changingCells.onchange.calcExp();
+
+            // And on click with the submitButton element triggers the validateSummary function.
+            document.getElementById("submitButton").click.validateSummary();
+
+      };
+
+});
+
+// The function, validateSummary, is established that has the following commands:
+function validateSummary() {
+
+      // A condition is set that if changingCells value is missing within the validity, then a message prompt will appear telling the users as such to include a summary of the trip in their report.
+      if (changingCells.validity.valueMissing) {
+
+            changingCells.setCustomValidity("You must include a summary of the trip in your report.");
+
+
+            // Otherwie, however, and the customValidity is set to an empty string.
+      } else {
+
+            changingCells.setCustomValidity("");
+
+      }
+
+      // The calcClass is declared with sumClass being set as a parameter that does the following:
+      function calcClass(sumClass) {
+
+            // sumFields as a variable is set to the sumClass elements' values.
+            var sumFields = document.getElementsByClassName("sumClass");
+
+            // sumTotal also as a local variable is set to a numerical value of zero.
+            var sumTotal = 0;
+
+            // And a for loop is created running through each item in sumFields that for each one...
+            for (var i = 0; i < sumFields.length; i++) {
+
+                  // Creates the local itemValue variable that takes the input elements' values from the sumFields variable. 
+                  var itemValue = parseFloat(sumFields.getElementsByTagName("input"));
+
+                  // If itemValue is a nurmerical value them it adds itself together.
+                  if (isNaN(itemValue)) {
+
+                        itemValue + itemValue;
+
+                  }
+
+            };
+
+            // Lastly here, the value of sumTotal is returned. 
+            return sumTotal;
+
+      }
+
+      // The calcExp function is established that sets out to...
+      function calcExp() {
+
+            // Set the local varaible of expTable to the table row elements within the travelExp table.
+            var expTable = document.travelExp.getElementsByTagName("tr");
+
+            // A for loop is made to each collection of expTable items that sets the input elements' subtotal item to the date item processed through the calcClass function and is formatted to have two digits as well.
+            for (var i = 0; i < expTable.length; i++) {
+
+                  document.input.getElementById("subtotal" + [i]) = calcClass(date + [i]).formatNumber(2);
+
+            };
+
+            // calcClass is ran with the following parameters shown below that are also made into two digits values.
+            calcClass("trans", "lodge", "meal", "other").formatNumber(2);
+
+            // Finally, global variable expTotal is set to the value of the calcClass result ran with "sum" as a parameter and formatted into the standard United States currency way.
+            expTotal = calcClass("sum").formatUSCurrency();
+
+      }
+
+}
 
 
 
